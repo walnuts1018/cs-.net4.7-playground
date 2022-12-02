7721bccdfefe460ca841e696e26f9663b98dbd2c
walnuts1018 <r.juglans.1018@gmail.com>
2022-12-02 09:58:22 +0900

    Init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_.net4._7_playground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
