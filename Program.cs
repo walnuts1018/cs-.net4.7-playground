3dc6a261e9e73d8b23cb16c62bf805cbe2f58ff4
walnuts1018 <r.juglans.1018@gmail.com>
2022-12-07 21:34:46 +0900

    for

--- Program.cs
+++ Program.cs
@@ -12,22 +12,9 @@ namespace cs_.net4._7_playground
     {
         static void Main(string[] args)
         {
-            var score = 75;
-            if (score >= 90)
+            for (int i = 1; i < 10; i++)
             {
-                Console.WriteLine("優"); 
-            }
-            else if (score >=70)
-            {
-                Console.WriteLine("良");
-            }
-            else if (score >= 50)
-            {
-                Console.WriteLine("可");
-            }
-            else
-            {
-                Console.WriteLine("不可");
+                Console.WriteLine(i);
             }
         }
     }
