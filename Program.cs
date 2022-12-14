8be718ee98ae0f4718e79a9dc99108b69b254523
walnuts1018 <r.juglans.1018@gmail.com>
2022-12-14 23:35:23 +0900

    add

--- Program.cs
+++ Program.cs
@@ -7,21 +7,28 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml;
-
-
+using System.IO;
 namespace cs_.net4._7_playground
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var str = "仕事用はwings@example.jpです。 プライベート 用 は YAMA@example.com です。";
-            var rgx = new Regex(@"[ a-z 0-9.!#$%&'*+/=?^_{|}~-]+@[ a-z 0-9-]+(?:\.[a-z 0-9-]+)*", RegexOptions.IgnoreCase);
-            var result = rgx.Matches(str);
-            foreach (Match m in result)
-            {
-                Console.WriteLine(m.Value);
-            }
+            var file = new FileInfo(@"C:\Users\juglans\dev\cs-.net4.7-playground\sample\SelfCSharp\Chap05\sample.txt");
+            Console.WriteLine(file.Exists);
+            Console.WriteLine(file.Name);
+            Console.WriteLine(file.Name);
+            Console.WriteLine(file.DirectoryName);
+
+            Console.WriteLine(file.IsReadOnly);
+            Console.WriteLine(file.LastAccessTime);
+            Console.WriteLine(file.LastWriteTime);
+            Console.WriteLine(file.Length);
+
+            var file2 = file.CopyTo(@"C:\Users\juglans\dev\cs-.net4.7-playground\sample\SelfCSharp\Chap05\sample_copy.txt",overwrite: true);
+            file2.MoveTo(@"C:\Users\juglans\sample_copy.txt");
+            // file2.Delete();
+
         }
     }
 }
